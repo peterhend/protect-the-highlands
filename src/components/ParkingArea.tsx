09670ede0bb985d536c62148023eed87df9730f1
Peter Henderson <12935421+peterhend@users.noreply.github.com>
2023-02-27 07:32:52 -0500

    Updates

--- src/components/ParkingArea.tsx
+++ src/components/ParkingArea.tsx
@@ -10,27 +10,33 @@ import LocalParkingIcon from "@mui/icons-material/LocalParking";
 
 interface ParkingAreaProps {
   name: string;
-  roadLeft?: boolean
-  roadRight?: boolean
+  roadLeft?: boolean;
+  roadRight?: boolean;
 }
 
-export const ParkingArea: FC<ParkingAreaProps> = ({ name, roadLeft, roadRight }) => {
+export const ParkingArea: FC<ParkingAreaProps> = ({
+  name,
+  roadLeft,
+  roadRight,
+}) => {
   return (
     <Grid item xs={1}>
       <Box
         sx={{
           bgcolor: "white",
           height: 140,
-          ml: roadLeft? 0 : 1,
-          mr: roadRight? 0 : 1,
+          ml: roadLeft ? 0 : 1,
+          mr: roadRight ? 0 : 1,
           borderLeft: roadLeft ? "8px solid #333" : null,
-          borderRight: roadRight? "8px solid #333" : null,
+          borderRight: roadRight ? "8px solid #333" : null,
         }}
       >
-        <CardHeader title={name} />
-        <CardContent>
-          <Avatar sx={{ bgcolor: "black" }}>P</Avatar>
-        </CardContent>
+        <Card sx={{height: 140}}>
+          <CardHeader title={name} />
+          <CardContent>
+            <Avatar sx={{ bgcolor: "black" }}>P</Avatar>
+          </CardContent>
+        </Card>
       </Box>
     </Grid>
   );
